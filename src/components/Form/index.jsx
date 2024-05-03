44e4b68673e99568a740c0e5e817683d9f60d80b
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-05-03 16:12:31 -0300

    feat: creating RegisterForm component

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,5 +1,59 @@
+import {
+  ArrowBack,
+  Container,
+  DataContact,
+  Header,
+  Button,
+  Navigate,
+} from "./styles";
+
+import { useNavigate } from "react-router-dom";
+
 const Form = () => {
-  return <h1>teste</h1>;
+  const navigate = useNavigate();
+
+  const backToHome = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      <Container>
+        <ArrowBack onClick={backToHome}>
+          <span class="material-symbols-outlined">arrow_back_ios</span>
+        </ArrowBack>
+        <Header>
+          <span class="material-symbols-outlined">how_to_reg</span>
+          <h1>Dados do Contato</h1>
+        </Header>
+        <label htmlFor="contactName">Nome Completo</label>
+        <DataContact>
+          <input type="text" id="contactName" />
+          <span class="material-symbols-outlined">contacts_product</span>
+        </DataContact>
+        <label htmlFor="email">E-mail</label>
+        <DataContact>
+          <input type="email" id="email" />
+          <span class="material-symbols-outlined">alternate_email</span>
+        </DataContact>
+        <label htmlFor="tel">Tel</label>
+        <DataContact>
+          <input type="tel" id="tel" />
+          <span class="material-symbols-outlined">call</span>
+        </DataContact>
+
+        <Navigate>
+          <Button onClick={backToHome}>
+            Cancelar
+            <span class="material-symbols-outlined">cancel</span>
+          </Button>
+          <Button register>
+            Cadastrar
+            <span class="material-symbols-outlined">app_registration</span>
+          </Button>
+        </Navigate>
+      </Container>
+    </>
+  );
 };
 
 export default Form;
---
+++ src/components/Form/styles.js
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+import variaveis from "../../styles/variaveis";
+
+export const Container = styled.div`
+  font-family: nunito, sans-serif;
+  label {
+    cursor: pointer;
+  }
+`;
+
+export const ArrowBack = styled.div`
+  span {
+    cursor: pointer;
+  }
+  @media (max-width: 768px) {
+    position: fixed;
+    top: 20px;
+    left: 40px;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  color: ${variaveis.themeColor};
+
+  span {
+    font-size: 36px;
+  }
+
+  h1 {
+    text-align: center;
+  }
+`;
+
+export const DataContact = styled.div`
+  display: grid;
+  grid-template-columns: 90% auto;
+
+  span {
+    color: ${variaveis.themeColor};
+    display: block;
+  }
+
+  input {
+    font-size: 14px;
+    font-weight: bold;
+    margin-bottom: 12px;
+    border: none;
+    border-bottom: 1px solid ${variaveis.themeColor};
+    outline: none;
+    height: 36px;
+    color: ${variaveis.contactText};
+
+    &:focus {
+      border-bottom: 3px solid ${variaveis.themeColor};
+      transition: border-bottom 0.1s;
+    }
+  }
+`;
+
+export const Navigate = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px auto 0;
+  width: 230px;
+`;
+
+export const Button = styled.button`
+  background-color: ${(props) =>
+    props.register ? `${variaveis.themeColor}` : "red"};
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border-radius: 12px;
+  font-weight: bold;
+  color: #fff;
+  border: 1px solid #fff;
+  cursor: pointer;
+
+  span {
+    margin-left: 4px;
+  }
+`;
